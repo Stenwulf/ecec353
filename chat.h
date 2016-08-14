0ee239a2023e5c97d34b52652ab4a7d7a21e935a
Weigel <ekw47@xunil-04.coe.drexel.edu>
2016-08-14 16:25:33 -0400

    Committer: Weigel <ekw47@xunil-04.coe.drexel.edu>
     On branch master
    
     Initial commit
    
     Changes to be committed:
       (use "git rm --cached <file>..." to unstage)
    
    	new file:   chat.h
    	new file:   runscript
    	new file:   server.c

---
+++ chat.h
@@ -0,0 +1,32 @@
+#ifndef CHAT_H
+#define CHAT_H
+
+
+#define MESSAGE_SIZE 1024
+
+// Group Context
+//    group_id - Containts the group id that the client will connect to
+//    client_id - Name/ID of the client that is connecting to the group
+struct group_context{
+
+   char* group_id;
+   char* client_id;  
+
+};
+
+// Message
+//    client_id - Name/ID of the client that is sending the message
+//    message_text - THe contents of the message being sent
+//
+//    peer_flag - Flag to indicate if the message should be a whisper
+struct message_struct{
+
+   char* client_id;
+   char* message_text;
+
+   int peer_flag;
+
+};
+
+
+#endif
